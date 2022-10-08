f40e21ddbde3780756e4451b59f5cf5620b14000
Dipanshu Raj <108553225+dpsdmh@users.noreply.github.com>
2022-10-09 03:11:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ function calculateTip() {
     //Calculate tip
     var total = (billAmt * serviceQual) / numOfPeople;
     //round to two decimal places
-    total = Math.round(total * 100) / 100;
+    total = Math.round(total * 1) / 100;
     //next line allows us to always have two digits after decimal point
     total = total.toFixed(2);
     //Display the tip
